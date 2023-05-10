f67008067c7c2c7f8ff7cd23fad86711b9d373c7
ElkinAndrey <andreyridbir@gmail.com>
2023-05-10 16:28:40 +0500

    Добавил интерфейс сервиса приложения

---
+++ BreedFoodStoreListopad.Service/Abstractions/IService.cs
@@ -0,0 +1,20 @@
+﻿using BreedFoodStoreListopad.Domain.Entities;
+
+namespace BreedFoodStoreListopad.Service.Abstractions
+{
+    /// <summary>
+    /// Интерфейс сервиса приложения
+    /// </summary>
+    internal interface IService
+    {
+        /// <summary>
+        /// Добавить новую категорию
+        /// </summary>
+        /// <param name="name">Название категории</param>
+        /// <param name="fileName">Название файла</param>
+        /// <param name="contentType">MIMO тип файла. Например, "image/png"</param>
+        /// <param name="stream">Стрим, который будет записан в файл</param>
+        /// <returns></returns>
+        public Task AddCategoryAsync(string name, string fileName, string contentType, Stream stream);
+    }
+}

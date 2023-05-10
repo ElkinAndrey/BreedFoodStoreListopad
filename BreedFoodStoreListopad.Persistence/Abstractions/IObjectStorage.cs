70754b51441c7313d842bcf9c427eaf861997798
ElkinAndrey <andreyridbir@gmail.com>
2023-05-10 18:09:43 +0500

    Добавил интерфейс для доступа к объектному хранилищу

---
+++ BreedFoodStoreListopad.Persistence/Abstractions/IObjectStorage.cs
@@ -0,0 +1,18 @@
+﻿namespace BreedFoodStoreListopad.Persistence.Abstractions
+{
+    /// <summary>
+    /// Интерфейс объектного хранилища файлов
+    /// </summary>
+    public interface IObjectStorage
+    {
+        /// <summary>
+        /// Добавить новый файл в объектное хранилище
+        /// </summary>
+        /// <param name="folder">Папка, в которой будет хранится файл. Например, "folder/subfolder"</param>
+        /// <param name="fileName">Имя добавляемого файла. Например "123.jpg"</param>
+        /// <param name="contentType">MIMO тип файла. Например, "image/png"</param>
+        /// <param name="stream">Стрим, который будет записан в файл</param>
+        /// <returns></returns>
+        public Task AddFile(string folder, string fileName, string contentType, Stream stream);
+    }
+}

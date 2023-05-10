6ca6ad1a9cee39c714f0a162865bdf2b59b331c1
ElkinAndrey <andreyridbir@gmail.com>
2023-05-10 18:12:13 +0500

    Добавил класс-руководитель для управления хранилищами

---
+++ BreedFoodStoreListopad.Persistence/Abstractions/IRepositoryManager.cs
@@ -0,0 +1,18 @@
+﻿namespace BreedFoodStoreListopad.Persistence.Abstractions
+{
+    /// <summary>
+    /// Интерфейс руководителя хранилищами, содержит в себе все хранилища
+    /// </summary>
+    public interface IRepositoryManager
+    {
+        /// <summary>
+        /// Хранилище приложение для хранения данных
+        /// </summary>
+        public IRepository Repository { get; }
+
+        /// <summary>
+        /// Объектное хранилище приложение для хранения файлов
+        /// </summary>
+        public IObjectStorage ObjectStorage { get; }
+    }
+}
---
+++ BreedFoodStoreListopad.Persistence/Repositories/FakeRepositoryManager.cs
@@ -0,0 +1,33 @@
+﻿using BreedFoodStoreListopad.Persistence.Abstractions;
+
+namespace BreedFoodStoreListopad.Persistence.Repositories
+{
+    /// <summary>
+    /// Фейковый руководитель хранилищами
+    /// </summary>
+    public class FakeRepositoryManager : IRepositoryManager
+    {
+        /// <summary>
+        /// Хранилище приложение для хранения данных
+        /// </summary>
+        private readonly IRepository _repository;
+
+        /// <summary>
+        /// Объектное хранилище приложение для хранения файлов
+        /// </summary>
+        private readonly IObjectStorage _objectStorage;
+        
+        /// <summary>
+        /// Фейковый руководитель хранилищами
+        /// </summary>
+        public FakeRepositoryManager()
+        {
+            _repository = new FakeRepository();
+            _objectStorage = new FakeObjectStorage();
+        }
+
+        public IRepository Repository => _repository;
+
+        public IObjectStorage ObjectStorage => _objectStorage;
+    }
+}
